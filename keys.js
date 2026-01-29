c390e8cf87a4b84671667422360fd4412e9e8a78
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-30 01:41:50 +0600

    accessing keys of object

---
+++ keys.js
@@ -0,0 +1,14 @@
+const computer = {
+    brand: 'Asus',
+    price: 80000,
+    SSD: '512GB',
+    monitor: 'Asus XD',
+    model: 'A16',
+    processor: 'intel core i-5'
+}
+
+//all keys as an array 
+const features = Object.keys(computer);
+console.log(features);
+
+
