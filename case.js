c0fd8798c91def24ad0e2ebef3a7d8ea5962c8ca
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-29 17:39:59 +0600

    trim and case-convertion in js

--- case.js
+++ case.js
@@ -1,14 +1,30 @@
-const school = 'raj uk uttara model school';
+const school = 'Raj UK Uttara Model School';
 
 console.log(school);
+console.log(school.toLowerCase()); //converts all to lower case
 
-const subject = 'Chemistry';
-const book = 'chemistry';
+const subject = 'Chemistry'; //uppercase
+const book = 'chemistry'; //case sensitive - not same
 
-if(subject == book)
+console.log(book.toUpperCase()); //converts all to upper case
+
+//uppercase - ABCD EFG 
+//lowercase - abcd efg 
+if(subject === book)
 {
     console.log("I am reading book, eibar exam a fatay felbo");
 }
 else{
     console.log("hudai hudai page ultai ar vat khai");
+}
+
+const drink = 'water';
+const liquid = '    water    ';
+//initially not the same
+
+if(drink.trim() === liquid.trim()){
+    console.log('pani khaite parbo');
+}
+else{
+    console.log('pani ase but khaite pari na');
 }
---
+++ question.js
@@ -0,0 +1,28 @@
+let str1 = "Mississippi";
+console.log(str1.indexOf("i", 3));
+
+
+const myObject = {
+ a: 1,
+ b: 2,
+ c: 3
+};
+
+for (let prop in myObject) {
+ console.log(myObject[prop]);
+}
+
+
+
+const x = ""
+if(!x.length){
+console.log("Hey")
+}else{
+console.log("Hi")
+}
+
+
+
+const str = "Hello";
+str[0] = "h";
+console.log(str);
