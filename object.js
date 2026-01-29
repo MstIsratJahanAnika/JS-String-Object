29aa25d81901382939a816a798cb2596a17e3cd2
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-29 20:47:43 +0600

    about object

---
+++ object.js
@@ -0,0 +1,29 @@
+//primitive - basic/primary 
+const age = 21;
+const college = 'noor mohammad public college';
+const isPassed = true;
+let isDefined;
+const subjects = ['bangla', 'english', 'math', 'chemistry', 'physics'];
+
+
+//object
+//non-primitive - mix of object inside object, constants, array
+const bottle = {
+    //each property syntax: key: value, (, is must after every key element)
+    brand: 'orange',
+    color: 'transparent', //each key is variable 
+    cost: 30,
+    availability: 'specific',
+    isWorth: true
+}
+
+const subject ={
+    name: 'physics',
+    instructor: 'sania akon',
+    examDate: '31 Jan',
+    chapter:['first', 'third', 'forth', 'eleven'],
+    exam: {
+        type: 'Final Exam',
+        marks: 100
+    }
+}
