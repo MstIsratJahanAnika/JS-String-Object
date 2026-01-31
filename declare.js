e7814280208c259a5de6080957316fdaa8ea91cf
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-01-31 20:15:40 +0600

    declaration/create object

---
+++ declare.js
@@ -0,0 +1,9 @@
+//one line object declaration 
+const pen = { brand: 'econo', price: 10, color: 'black'};
+const pencil = new Object(); //object declare 
+console.log(pencil);
+
+const rubber = Object.create({}); //empty object 
+console.log(rubber);
+
+//using class or function 
